47eab25ad4b6f32d899f73d62760af64b091d6e4
BaekHaengLee@gmail.com <BaekHaengLee@gmail.com@db9728ea-e686-615b-07e7-379d3d374726>
2012-12-23 14:37:01 +0000

    모바일용 공통컴포넌트 포팅 및 사용자 지원 템플릿

---
+++ trunk/egovframe/2.5.0/user-support-webapp/src/main/java/egovframework/mbl/com/mgr/web/EgovMobileMenuController.java
@@ -0,0 +1,257 @@
+package egovframework.mbl.com.mgr.web;
+
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springmodules.validation.commons.DefaultBeanValidator;
+
+import egovframework.com.cmm.LoginVO;
+import egovframework.com.cmm.util.EgovUserDetailsHelper;
+import egovframework.mbl.com.mgr.service.MenuVO;
+import egovframework.mbl.com.mgr.service.EgovMobileMenuService;
+import egovframework.rte.fdl.idgnr.EgovIdGnrService;
+
+/**
+ * @Class Name : EgovMobileMenuController.java
+ * @Description : 모바일 템플릿을 위한 Controller
+ * @Modification Information
+ * 
+ *   수정일                수정자              수정내용
+ *  -------       --------    ---------------------------
+ *  2011.08.23    최서윤              최초 생성
+ *
+ *  @author 모바일 개발환경 개발팀 최서윤
+ *  @since 2011.08.23
+ *  @version 1.0
+ *  
+ */
+
+@Controller
+public class EgovMobileMenuController {
+	
+	protected Log log = LogFactory.getLog(this.getClass());
+	
+	@Autowired
+	private DefaultBeanValidator beanValidator;
+	
+	@Resource(name="EgovMobileMenuService")
+	private EgovMobileMenuService mobileMenuService;
+
+	@Resource(name = "egovMobileMenuIdGnrService")
+    private EgovIdGnrService idgenService;
+		
+	/**
+	 * 첫번째 메인 menu화면 출력
+	 * 
+	 * @param MenuVO
+     * @return EgovMainMenu화면
+     * @throws Exception
+	 * */
+	@RequestMapping("/mgr/EgovMainMenu.mdo")
+	public String showFirstPage(@ModelAttribute("menuVO") MenuVO vo, @ModelAttribute("loginVO") LoginVO loginVO,
+								HttpServletRequest request, ModelMap model) throws Exception {
+		
+		Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
+		
+		if (!isAuthenticated ){
+			return "egovframework/mbl/com/uat/uia/EgovLoginUsr";
+		}
+		LoginVO loginVo = (LoginVO) request.getSession().getAttribute("loginVO");
+		
+		Map<String, Object> map = mobileMenuService.selectUseList(vo);
+
+		model.addAttribute("resultList", map.get("resultList"));
+		model.addAttribute("loginVO", loginVo);
+		
+		return "egovframework/mbl/com/mgr/EgovMainMenu";
+	}
+	
+	
+	/**
+	 * db 내에 생성한 목록 조회
+	 * 
+	 * @param MenuVO
+	 * @return EgovMenuList화면
+     * @throws Exception
+	 * */
+	@RequestMapping("/mgr/EgovMenuList.mdo")
+	public String selectMenuList(@ModelAttribute("menuVO") MenuVO vo, @ModelAttribute("loginVO") LoginVO loginVO,
+			 					 HttpServletRequest request, ModelMap model) throws Exception {
+		
+		LoginVO loginVo = (LoginVO) request.getSession().getAttribute("loginVO");
+		Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
+		
+		if (!isAuthenticated || !"USR".equals(loginVo.getUserSe())){
+			return "forward:/index.jsp";
+		}
+		
+		Map<String, Object> map = mobileMenuService.selectMenuList(vo);
+		
+		model.addAttribute("resultList", map.get("resultList"));
+		model.addAttribute("loginVO", loginVo);
+		
+		return "egovframework/mbl/com/mgr/EgovMenuList";
+	}
+	
+	
+	/**
+	 * 생성한 Menu목록 중 선택한 사항에 대한 상세조회 
+	 * 
+	 * @param MenuVO
+	 * @return EgovMenuListDetail화면
+     * @throws Exception
+	 * */
+	@RequestMapping("/mgr/EgovMenuListDetail.mdo")
+	public String selectMenuDetail(@ModelAttribute("menuVO") MenuVO vo, @ModelAttribute("loginVO") LoginVO loginVO,
+			 						HttpServletRequest request, ModelMap model) throws Exception {
+		
+		LoginVO loginVo = (LoginVO) request.getSession().getAttribute("loginVO");
+		Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
+		
+		if (!isAuthenticated || !"USR".equals(loginVo.getUserSe())){
+			return "forward:/index.jsp";
+		}
+		
+		MenuVO copVo = mobileMenuService.selectMenuDetail(vo);
+		model.addAttribute("result", copVo); 
+		
+		return "egovframework/mbl/com/mgr/EgovMenuListDetail";				
+	}	
+	
+	/**
+	 * 상세조회에서 수정페이지로 이동
+	 * 
+	 * 
+	 * */
+	@RequestMapping("/mgr/EgovMenuUpdatePage.mdo")
+	public String updateMenuListPage(@ModelAttribute("menuVO") MenuVO vo, @ModelAttribute("loginVO") LoginVO loginVO,
+			 						  HttpServletRequest request, ModelMap model) throws Exception {
+
+		LoginVO loginVo = (LoginVO) request.getSession().getAttribute("loginVO");
+		Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
+		
+		if (!isAuthenticated || !"USR".equals(loginVo.getUserSe())){
+			return "forward:/index.jsp";
+		}
+		
+		Map<String, Object> map = mobileMenuService.selectUpperMenuList(vo);
+		model.addAttribute("resultList", map.get("resultList"));
+		model.addAttribute("resultUpdate", vo);
+		
+		return "egovframework/mbl/com/mgr/EgovMenuUpdate";
+	}
+	
+	/**
+	 * 상세조회한 내용 수정 
+	 * 
+	 * @param MenuVO
+	 * @return EgovMenuList화면
+     * @throws Exception
+	 * */
+	@RequestMapping("/mgr/EgovMenuUpdate.mdo")
+	public String updateMenuList(@ModelAttribute("menuVO") MenuVO vo, @ModelAttribute("loginVO") LoginVO loginVO,
+			 					  HttpServletRequest request, ModelMap model) throws Exception {
+		
+		LoginVO loginVo = (LoginVO) request.getSession().getAttribute("loginVO");
+		Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
+		
+		if (!isAuthenticated || !"USR".equals(loginVo.getUserSe())){
+			return "forward:/index.jsp";
+		}
+
+		String lastUpdusrId = loginVo.getId();
+		vo.setLastUpdusrId(lastUpdusrId);
+		mobileMenuService.updateMenu(vo);
+		
+		return "forward:/mgr/EgovMenuList.mdo";
+	}
+    
+    /**
+	 * 등록 페이지로 이동
+	 * 
+	 * @return EgovMenuRegist화면
+     * @throws Exception
+	 * */
+	@RequestMapping("/mgr/EgovMenuRegistPage.mdo")
+	public String registMenuListPage(@ModelAttribute("menuVO") MenuVO copVO, @ModelAttribute("loginVO") LoginVO loginVO,
+									  HttpServletRequest request, ModelMap model) throws Exception{
+		
+		LoginVO loginVo = (LoginVO) request.getSession().getAttribute("loginVO");
+		Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
+		
+		if (!isAuthenticated || !"USR".equals(loginVo.getUserSe())){
+			return "forward:/index.jsp";
+		}
+		
+		Map<String, Object> map = mobileMenuService.selectUpperMenuList(copVO);
+		model.addAttribute("resultList", map.get("resultList"));
+		
+		return "egovframework/mbl/com/mgr/EgovMenuRegist";
+	}
+	
+	/**
+	 * 등록 로직
+	 * 
+	 * @return EgovMenuList화면
+     * @throws Exception
+	 * */
+	@RequestMapping("/mgr/EgovMenuRegist.mdo")
+	public String registMenuList(@ModelAttribute("menuVO") MenuVO vo, @ModelAttribute("loginVO") LoginVO loginVO,
+								 BindingResult bindingResult, HttpServletRequest request,
+					             ModelMap model) throws Exception {
+
+		LoginVO loginVo = (LoginVO) request.getSession().getAttribute("loginVO");
+		Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
+		
+		if (!isAuthenticated || !"USR".equals(loginVo.getUserSe())){
+			return "forward:/index.jsp";
+		}
+		
+		//서버  validate 체크
+        beanValidator.validate(vo, bindingResult);
+		if(bindingResult.hasErrors()){
+	        return "egovframework/mbl/com/mgr/EgovMenuRegist";
+		} 
+
+		String frstRegisterId = loginVo.getId();
+		vo.setFrstRegisterId(frstRegisterId);
+		
+		vo.setMenuCode(idgenService.getNextStringId()); //메뉴코드 set
+		mobileMenuService.insertMenu(vo);
+		
+		return "forward:/mgr/EgovMenuList.mdo";
+	}
+	
+	/**
+	 * 삭제 요청시 삭제
+	 * 
+	 * @param MenuVO
+	 * @return EgovMenuList화면
+     * @throws Exception
+	 * */
+	@RequestMapping("/mgr/EgovDelete.mdo")
+	public String deleteMenu(@ModelAttribute("menuVO") MenuVO vo, @ModelAttribute("loginVO") LoginVO loginVO,
+							  HttpServletRequest request, ModelMap model) throws Exception {
+
+		LoginVO loginVo = (LoginVO) request.getSession().getAttribute("loginVO");
+		Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
+		
+		if (!isAuthenticated || !"USR".equals(loginVo.getUserSe())){
+			return "forward:/index.jsp";
+		}
+		
+		mobileMenuService.deleteMenu(vo);	
+		
+		return "forward:/mgr/EgovMenuList.mdo";
+	}
+}
---
+++ trunk/egovframe/2.5.0/user-support-webapp/src/main/webapp/WEB-INF/jsp/egovframework/mbl/com/mgr/EgovMainMenu.jsp
@@ -0,0 +1,128 @@
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%--
+  Class Name : EgovMainMenu.jsp
+  Description :  menu list
+  Modification Information
+
+      수정일         수정자           수정내용
+    -------------    --------    ---------------------
+     2011.08.23      최서윤         최초 생성
+     2011.09.15      구지연         수정
+ 
+    author   : 모바일 개발환경 개발팀 최서윤
+    since    : 2011.08.23
+--%>
+<!DOCTYPE html> 
+<html> 
+  <head> 
+        <title>eGovframe Mobile Framework</title> 
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  
+        <!-- eGovFrame Common import -->
+        <link href="<c:url value='/css/egovframework/mbl/cmm/jquery.mobile-1.1.1.min.css' />" rel="stylesheet" type="text/css" />
+        <link href="<c:url value='/css/egovframework/mbl/cmm/EgovMobile-1.1.1.css' />" rel="stylesheet" type="text/css" />
+        <script type="text/javascript" src="<c:url value='/js/egovframework/mbl/cmm/jquery-1.7.1.min.js' />"></script>
+        <script type="text/javascript" src="<c:url value='/js/egovframework/mbl/cmm/jquery.mobile-1.1.1.min.js' />"></script>
+		<script type="text/javascript" src="<c:url value='/js/egovframework/mbl/cmm/EgovMobile-1.1.1.js' />"></script>
+   
+        <!-- 개발환경 import -->
+        <link href="<c:url value='/css/egovframework/mbl/com/dev/egovDev.css' />" rel="stylesheet" type="text/css" />
+        
+        <script type="text/javascript">
+
+	        function fn_egov_forward_upperMenuId() {
+
+				document.menuVO.action = "<c:url value='/mgr/EgovMenuList.mdo' />";
+				document.menuVO.submit();
+			}
+        
+			function fn_egov_forward_menu(cnncUrl) {
+				
+				//document.menuVO.action = "<c:url value='"+cnncUrl+"'/>";
+				document.menuVO.action = "${pageContext.request.contextPath}"+cnncUrl;
+				document.menuVO.submit();
+			}
+		</script>
+	</head>
+	
+<body> 
+<div data-role="page">
+	<div data-role="header" data-position="inline" data-theme="z">
+	   <h1><img src="<c:url value='/images/egovframework/mbl/bod/logo.png' />"  /></h1>
+	</div>
+	<form name="menuVO" method="post" action="">
+       <input type="hidden" name="menuCode" value="<c:out value='${result.menuCode}'/>" >
+        <input type="hidden" name="menuNm" value="<c:out value='${result.menuNm}'/>" >
+   
+        <input type="hidden" name="cnncUrl" value="<c:out value='${result.cnncUrl}'/>" >
+        <input type="hidden" name="menuDc" value="<c:out value='${result.menuDc}'/>" >
+        <input type="hidden" name="acyvtyAt" value="<c:out value='${result.acyvtyAt}'/>" >
+        <input type="hidden" name="upperMenuId" value="<c:out value='${result.upperMenuId}'/>" >
+		<div data-role="content" data-theme="d">	
+					
+			<div class="content-primary">
+				<ul data-role="listview" data-inset="true" >
+					<li data-role="list-divider" class="egov-bar-main3"><spring:message code="mgr.mobileEgov" /></li>
+					<li><p>모바일 전자정부 공통컴포넌트 메뉴</p></li>
+				</ul>				
+					<div data-role="collapsible-set">
+					<% int cnt=0; %>
+					<c:forEach var="result" items="${resultList}" varStatus="status">
+	
+						<c:if test="${result.menuLvl == '1'}">
+						<% cnt++; 
+						   if(cnt != 1){
+						%>
+							</div> <!-- collapsible Lv1 -->
+						<%
+						   }
+						%>
+						<div data-role="collapsible" data-collapsed="true" style="width:95%; margin-left:auto; margin-right:auto;" >
+							<h3><c:out value="${result.menuNm}"/></h3>
+							 <ul data-role="listview" data-inset="true" data-theme="d"> 
+						</c:if>
+								
+						<c:if test="${result.menuLvl == '2'}">
+							<li>
+								<a href="javascript:fn_egov_forward_menu('${result.cnncUrl}');" data-ajax="false">
+								<c:out value="${result.menuNm}"/>
+								</a>
+							</li>
+						</c:if>
+							
+					</c:forEach>
+							</ul>
+					</div>
+					
+					<c:if test="${loginVO.userSe == 'USR'}">
+					<div data-role="collapsible" data-collapsed="true" style="width:95%; margin-left:auto; margin-right:auto;" >
+					<h3><spring:message code="mgr.menuMngt" /></h3>
+						<ul data-role="listview" data-inset="true" data-theme="d">
+						<% int count=0; %>
+						<c:forEach var="result" items="${resultList}" varStatus="status">
+							<c:if test="${result.menuLvl != '1'}">
+							<% count++; %>
+							</c:if>
+						</c:forEach>
+							<li><a href="javascript:fn_egov_forward_upperMenuId();" data-ajax="false"><spring:message code="mgr.menuList" /><span class="ui-li-count"><%=count %></span></a></li>
+						</ul>
+					</div>
+					</c:if>
+
+			</div><!--/content-primary-->	
+		</div>
+
+	</form>
+	
+	<!-- footer start -->
+	<div data-role="footer" data-position="fixed">
+		<h1>Copyright (c) Ministry of Public Administration and Security.</h1>
+	</div>
+	<!-- footer end -->
+	
+</div>
+
+</body>
+</html>
